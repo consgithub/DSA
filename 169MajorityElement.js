3cf71b1036ec7090dc37e5c9c4f543a2a85b4b45
consgithub <cultopower@gmail.com>
2025-08-18 16:38:08 +1000

    26 added

--- 169MajorityElement.js
+++ 169MajorityElement.js
@@ -116,3 +116,51 @@ var majorityElement = function(nums) {
     // The remaining candidate is the majority element
     return candidate;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count = 0;
+    let candidate = null;
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+    return candidate;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count = 0;
+    let candidate = null;
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+    return candidate;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let count = 0;
+    let candidate = null;
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num = candidate) ? 1 : -1;
+    }
+    return candidate;
+};
