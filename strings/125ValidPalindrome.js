366cf6aaa46ead7aa0e3eb74bd9a3c8ac9ba03a0
consgithub <cultopower@gmail.com>
2025-08-20 21:10:04 +1000

    125 practice

--- strings/125ValidPalindrome.js
+++ strings/125ValidPalindrome.js
@@ -71,3 +71,63 @@ function isAlphanumeric(char) {
     .test(char) returns true if the character matches the pattern, false otherwise */
     return (/[a-z0-9]/i).test(char);
 }
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !isAlphanumeric(s[left])) {
+            left++;
+        };
+
+        while (left < right && !isAlphanumeric(s[right])) {
+            right--;
+        };
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        };
+        left++;
+        right--;
+    }
+    return true;
+};
+
+function isAlphanumeric(char) {
+    return (/[a-z0-9]/i).test(char);
+};
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !isAlphanumeric(s[left])) {
+            left++;
+        }
+
+        while (left < right && !isAlphanumeric(s[right])) {
+            right--;
+        }
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
+
+function isAlphanumeric(char) {
+    return (/[a-z0-9]/i).test(char);
+}
