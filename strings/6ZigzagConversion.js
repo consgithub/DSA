70022a713dbf5b901870aaa2d040f9a639f97a21
consgithub <cultopower@gmail.com>
2025-08-23 22:14:49 +1000

    6 practice

--- strings/6ZigzagConversion.js
+++ strings/6ZigzagConversion.js
@@ -54,5 +54,22 @@ var convert = function(s, numRows) {
  * @return {string}
  */
 var convert = function(s, numRows) {
-    
+    if (numRows === 1 || s.length <= numRows) {
+        return s;
+    }
+
+    let result = '';
+
+    let cycleLen = 2 * numRows - 2;
+
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j + i < s.length; j += cycleLen) {
+            result += s[j + i];
+
+            if (i !== 0 && i !== numRows - 1 && j + cycleLen - i < s.length) {
+                result += s[j + cycleLen - i];
+            }
+        }
+    }
+    return result;
 };
