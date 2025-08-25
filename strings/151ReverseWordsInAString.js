c7c056aaa9b03c7e711c549bd08c22d5a5c5e13d
consgithub <cultopower@gmail.com>
2025-08-25 12:57:38 +1000

    151 notes

--- strings/151ReverseWordsInAString.js
+++ strings/151ReverseWordsInAString.js
@@ -15,9 +15,14 @@ var reverseWords = function(s) {
  * @return {string}
  */
 var reverseWords = function(s) {
+    /* .trim() removes spaces before and after.
+    .split cuts the string into an array in the pattern you specify, the regex (/\s+/) finds a sequence of 
+    one or more whitespace characters. So the parts between the cuts become the array elements. */
     let words = s.trim().split(/\s+/);
 
+    // Reverses the order of the terms in the array
     words.reverse();
 
+    // Joins the terms in the array back into a string with a single space in between each
     return words.join(' ');
 };
