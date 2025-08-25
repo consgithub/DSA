bd50400039689c6baaa3dd9a9b620f6ddf442b4b
consgithub <cultopower@gmail.com>
2025-08-25 12:37:31 +1000

    151 complete

---
+++ strings/151ReverseWordsInAString.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let words = s.trim().split(/\s+/);
+
+    words.reverse();
+
+    return words.join(' ');
+};
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let words = s.trim().split(/\s+/);
+
+    words.reverse();
+
+    return words.join(' ');
+};
