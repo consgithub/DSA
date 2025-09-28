cd001c3684d1800a83489fd69417a2575c899454
consgithub <cultopower@gmail.com>
2025-09-28 14:51:19 +1000

    260 written again

--- bit manipulation/260SingleNumberIII.js
+++ bit manipulation/260SingleNumberIII.js
@@ -80,3 +80,26 @@ var singleNumber = function(nums) {
     // Return the unique number from each group
     return [num1, num2];
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var singleNumber = function(nums) {
+    let xor = 0;
+    for (let num of nums) {
+        xor ^= num;
+    }
+
+    let diffBit = xor & -xor;
+    let num1 = 0, num2 = 0;
+    for (let num of nums) {
+        if (num & diffBit) {
+            num1 ^= num;
+        }
+        else {
+            num2 ^= num;
+        }
+    }
+    return [num1, num2];
+};
