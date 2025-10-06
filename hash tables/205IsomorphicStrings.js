4c3c46d4a2ff5e0a910894bf75cb7ee293a2ebe6
consgithub <cultopower@gmail.com>
2025-10-06 14:40:51 +1100

    205 complete

--- hash tables/205IsomorphicStrings.js
+++ hash tables/205IsomorphicStrings.js
@@ -74,3 +74,26 @@ var isIsomorphic = function(s, t) {
     }
     return true;
 };
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let mapS = new Map();
+    let mapT = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        let charS = s[i];
+        let charT = t[i];
+
+        if ((mapS.has(charS) && mapS.get(charS) !== charT) || (mapT.has(charT) && mapT.get(charT) !== charS)) {
+            return false;
+        }
+
+        mapS.set(charS, charT);
+        mapT.set(charT, charS);
+    }
+    return true;
+};
