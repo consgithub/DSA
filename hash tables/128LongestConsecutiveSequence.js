5f1f79e656efb46f2f089e131c8967ebf7f179bc
consgithub <cultopower@gmail.com>
2025-10-10 12:40:14 +1100

    128 complete

--- hash tables/128LongestConsecutiveSequence.js
+++ hash tables/128LongestConsecutiveSequence.js
@@ -65,3 +65,26 @@ var longestConsecutive = function(nums) {
     // Return the longestStreak
     return longestStreak;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    let numSet = new Set(nums);
+    let longestStreak = 0;
+
+    for (let num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentStreak += 1;
+            }
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+    return longestStreak;
+};
